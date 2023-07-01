86c3bdb14c2f3c9def5d888baf0bd2c2cac0a6bc
tarunsheoran <118544285+tarunsheoran@users.noreply.github.com>
2023-07-01 21:12:06 +0530

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,10 @@
+print("My name is Tarun, \nI am a student at \"Lovely Professional University\".")
+# Just commenting
+'''A New Way
+To Cooment Multiple lines'''
+"""This will also work while Commenting"""
+
+print("A Great \'Engineer\'")
+print("Hey",5,87, sep="~", end="Error 404\n")
+print("Bye")
+print("Not End")
